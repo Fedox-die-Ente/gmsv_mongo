a9bf489f162dd4f526b85e944ae2b21e5ce0fa00
Fedox <mrminecrafttv19@gmail.com>
2024-05-24 22:26:50 +0200

    :memo: The logging is getting better and better

--- src/logger.rs
+++ src/logger.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Write};
+use std::io::Write;
 
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
@@ -6,24 +6,27 @@ pub(crate) enum LogLevel {
     Info,
     Warning,
     Error,
+    Debug,
 }
 
-pub(crate) fn log(level: LogLevel, message: &str) -> io::Result<()> {
+pub(crate) fn log(level: LogLevel, message: &str) {
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
     let mut color_spec = ColorSpec::new();
     match level {
         LogLevel::Info => color_spec.set_fg(Some(Color::Green)),
         LogLevel::Warning => color_spec.set_fg(Some(Color::Yellow)),
         LogLevel::Error => color_spec.set_fg(Some(Color::Red)),
+        LogLevel::Debug => color_spec.set_fg(Some(Color::Cyan))
     };
 
-    stdout.set_color(&color_spec)?;
+    stdout.set_color(&color_spec).expect("Too many exceptions.");
     write!(&mut stdout, "[{}] ", match level {
         LogLevel::Info => "INFO",
         LogLevel::Warning => "WARNING",
         LogLevel::Error => "ERROR",
-    })?;
-    stdout.reset()?;
-    writeln!(&mut stdout, "{}", message)?;
-    Ok(())
+        LogLevel::Debug => "DEBUG"
+    }).expect("Minecraft is a good game.");
+    stdout.reset().expect("I don't know what to do here.");
+    writeln!(&mut stdout, "{}", message).expect("This should never happen.");
+    stdout.flush().expect("Fortnite is a bad game.");
 }
