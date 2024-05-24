2e9402a11c3fd7097255b47f5258acc4cafd3ea8
Fedox <mrminecrafttv19@gmail.com>
2024-05-24 16:13:39 +0200

    Fixes..

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,22 @@ use logger::LogLevel;
 mod logger;
 mod mongo;
 
+#[lua_function]
+unsafe fn hello_world(lua: gmod::lua::State) -> i32 {
+    lua.get_global(lua_string!("print"));
+    lua.push_string("Hello, world!");
+    lua.call(1, 0);
+    return 0;
+}
+
 #[gmod13_open]
-fn gmod13_open(lua: gmod::lua::State) -> i32 {
+unsafe fn gmod13_open(lua: gmod::lua::State) -> i32 {
     log(LogLevel::Info, "GMSV_MONGO Binary loaded and ready.")
         .expect("Failed to log.");
+
+    lua.push_function(hello_world);
+    lua.set_global(lua_string!("hello_world"));
+
     return 0;
 }
 
--- src/mongo.rs
+++ src/mongo.rs
@@ -1,27 +1,50 @@
 #[cfg(test)]
 mod tests {
-    use std;
-    use std::process::exit;
-
-    use mongodb::Client;
+    use dotenv::dotenv;
+    use mongodb::{Client, Collection};
     use mongodb::options::{ClientOptions, ServerApi, ServerApiVersion};
 
-    use dotenv::dotenv;
+    use serde::{Deserialize, Serialize};
 
-    #[test]
+    use crate::logger::{log, LogLevel};
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct TestType {
+        name: String,
+        age: i32,
+    }
+
     #[tokio::test]
-    async fn test_connect() -> Result<(), Box<dyn std::error::Error>> {
+    async fn test_insert() -> mongodb::error::Result<()> {
         dotenv().ok();
-        let connection_string = std::env::var("MONGO_CONNECTION_STRING");
-        if connection_string.is_err() {
-            exit(1);
+
+        let connection_string = dotenv::var("MONGO_CONNECTION_STRING").expect("MONGODB_URI must be set");
+        if connection_string.is_empty() {
+            panic!("MONGO_CONNECTION_STRING must be set");
         }
 
         let mut client_options = ClientOptions::parse(connection_string).await?;
-
         let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
         client_options.server_api = Some(server_api);
 
         let client = Client::with_options(client_options)?;
+
+        let db = client.database("admin");
+
+        db.create_collection("testtest", None).await.expect("Failed to create collection");
+        log(LogLevel::Info, "Collection created.").expect("Failed to log.");
+
+        let collection: Collection<TestType> = db.collection("testtest");
+
+        let test = TestType {
+            name: "Test".to_string(),
+            age: 20,
+        };
+
+        collection.insert_one(test, None).await.expect("Failed to insert document");
+
+        collection.drop(None).await.expect("Failed to drop collection");
+
+        Ok(())
     }
 }
