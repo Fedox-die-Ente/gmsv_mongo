01df0130b379169768c65d413530fa1fc3a78ae8
Austria7 <lolmitmirxd@gmail.com>
2024-05-25 13:23:56 +0200

    [Functions] Use meta tables

--- src/functions/mongodbcollection.rs
+++ src/functions/mongodbcollection.rs
@@ -1,42 +1,24 @@
 #![allow(dead_code)]
 
+use mongodb::bson::Document;
+use crate::logger::{log, LogLevel};
+
 #[lua_function]
 pub unsafe fn insert(lua: gmod::lua::State) -> i32 {
-    let connection_url = lua.get_field(1, lua_string!("_connection_url"));
-    let database_name = lua.get_field(1, lua_string!("_database_name"));
-    let collection_name = lua.get_field(1, lua_string!("_collection_name"));
-    let document = lua.get_string(1).unwrap();
-
     return 1;
 }
 
 #[lua_function]
 pub unsafe fn find(lua: gmod::lua::State) -> i32 {
-    let connection_url = lua.get_field(1, lua_string!("_connection_url"));
-    let database_name = lua.get_field(1, lua_string!("_database_name"));
-    let collection_name = lua.get_field(1, lua_string!("_collection_name"));
-    let query = lua.get_string(1).unwrap();
-
     return 1;
 }
 
 #[lua_function]
 pub unsafe fn update(lua: gmod::lua::State) -> i32 {
-    let connection_url = lua.get_field(1, lua_string!("_connection_url"));
-    let database_name = lua.get_field(1, lua_string!("_database_name"));
-    let collection_name = lua.get_field(1, lua_string!("_collection_name"));
-    let query = lua.get_string(1).unwrap();
-    let update = lua.get_string(1);
-
     return 1;
 }
 
 #[lua_function]
 pub unsafe fn delete(lua: gmod::lua::State) -> i32 {
-    let connection_url = lua.get_field(1, lua_string!("connection_url"));
-    let database_name = lua.get_field(1, lua_string!("database_name"));
-    let collection_name = lua.get_field(1, lua_string!("collection_name"));
-    let query = lua.get_string(1).unwrap();
-
     return 1;
 }
--- src/functions/mongodbdatabase.rs
+++ src/functions/mongodbdatabase.rs
@@ -1,23 +1,13 @@
 #![allow(dead_code)]
 
 use crate::functions::mongodbcollection::{delete, find, insert, update};
+use crate::logger::{log, LogLevel};
 
 #[lua_function]
 pub unsafe fn get_collection(lua: gmod::lua::State) -> i32 {
-    let connection_url = lua.get_field(1, lua_string!("_connection_url"));
-    let database_name = lua.get_field(1, lua_string!("_database_name"));
+    lua.get_field(1, lua_string!("collection_name"));
     let collection_name = lua.get_string(1).unwrap();
-
-    lua.push_string(&*collection_name);
-    lua.set_field(-2, lua_string!("collection_name"));
-    lua.push_function(insert);
-    lua.set_field(-2, lua_string!("Insert"));
-    lua.push_function(find);
-    lua.set_field(-2, lua_string!("Find"));
-    lua.push_function(update);
-    lua.set_field(-2, lua_string!("Update"));
-    lua.push_function(delete);
-    lua.set_field(-2, lua_string!("Delete"));
+    log(LogLevel::Debug, &*format!("Collection Name: {}", collection_name));
 
     return 1;
 }
