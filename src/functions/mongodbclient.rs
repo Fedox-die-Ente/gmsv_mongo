882f80af8783365163d57cf5bd67aeb8cfd027d5
Austria7 <lolmitmirxd@gmail.com>
2024-05-25 14:22:53 +0200

    [MongoDBClient] Set userdata

--- src/functions/mongodbclient.rs
+++ src/functions/mongodbclient.rs
@@ -1,6 +1,8 @@
 use gmod::lua::LUA_REGISTRYINDEX;
+use mongodb::Client;
 
 use crate::logger::{log, LogLevel};
+use crate::mongo::connect_to_db;
 
 #[lua_function]
 pub unsafe fn new_client(lua: gmod::lua::State) -> i32 {
@@ -8,7 +10,12 @@ pub unsafe fn new_client(lua: gmod::lua::State) -> i32 {
     let connection_url = lua.get_string(1).unwrap();
     log(LogLevel::Debug, &*format!("{}", connection_url));
 
-    let metatable = lua.get_field(LUA_REGISTRYINDEX, lua_string!("MongoDBClient"));
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    let client = runtime.block_on(connect_to_db(&*connection_url)).unwrap();
+
+    lua.get_field(LUA_REGISTRYINDEX, lua_string!("MongoDBClient"));
+    let metatable = lua.get_metatable(-1);
+    lua.new_userdata(client, Option::from(metatable));
 
     return 1;
 }
@@ -21,5 +28,15 @@ pub unsafe fn get_database(lua: gmod::lua::State) -> i32 {
         LogLevel::Debug,
         &*format!("Database Name: {}", database_name),
     );
+
+    lua.get_field(LUA_REGISTRYINDEX, lua_string!("MongoDBClient"));
+    let client = lua.get_userdata::<Client>(-1).unwrap();
+    let db = client.database(database_name);
+    log(LogLevel::Debug, &*format!("Database from client: {:?}", db));
+
+    lua.get_field(LUA_REGISTRYINDEX, lua_string!("MongoDBDatabase"));
+    let metatable = lua.get_metatable(-1);
+    lua.new_userdata(db, Option::from(metatable));
+
     return 1;
 }
