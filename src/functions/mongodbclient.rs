378f0d954014041b1982c4b831d127c243705473
Fedox <mrminecrafttv19@gmail.com>
2024-05-25 13:25:27 +0200

    chore: Update .gitignore and mongodbclient.rs

--- src/functions/mongodbclient.rs
+++ src/functions/mongodbclient.rs
@@ -17,6 +17,9 @@ pub unsafe fn new_client(lua: gmod::lua::State) -> i32 {
 pub unsafe fn get_database(lua: gmod::lua::State) -> i32 {
     lua.get_field(1, lua_string!("database_name"));
     let database_name = lua.get_string(1).unwrap();
-    log(LogLevel::Debug, &*format!("Database Name: {}", database_name));
+    log(
+        LogLevel::Debug,
+        &*format!("Database Name: {}", database_name),
+    );
     return 1;
 }
