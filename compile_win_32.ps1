97e2948e25c84d2fb940ecef4c36d92e1f958a0b
Fedox <mrminecrafttv19@gmail.com>
2024-05-25 14:23:34 +0200

    feat: Remove and build gmsv_mongo_win32.dll in compile_win_32.ps1

--- compile_win_32.ps1
+++ compile_win_32.ps1
@@ -1,5 +1,28 @@
+function Write-BoldMessage {
+    param (
+        [string]$Message,
+        [string]$Color
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Write-Host "[ $timestamp ] " -NoNewline
+    Write-Host $Message -ForegroundColor $Color -NoNewline
+    Write-Host -NoNewline
+    Write-Host -NoNewline
+}
+
+if (Test-Path gmsv_mongo_win32.dll)
+{
+    Write-BoldMessage "Removing gmsv_mongo_win32.dll" "Red"
+    Write-Host -NoNewline
+    rm gmsv_mongo_win32.dll
+}
+
+Write-BoldMessage "Building gmsv_mongo_win32.dll" "DarkCyan"
 cargo build --release --target i686-pc-windows-msvc
+
 if (Test-Path target/i686-pc-windows-msvc/release/gmsv_mongo.dll)
 {
+    Write-BoldMessage "Renaming gmsv_mongo.dll to gmsv_mongo_win32.dll" "Yellow"
     mv target/i686-pc-windows-msvc/release/gmsv_mongo.dll gmsv_mongo_win32.dll
+    Write-BoldMessage "gmsv_mongo_win32.dll built successfully" "Green"
 }
--- compile_win_64.ps1
+++ compile_win_64.ps1
@@ -1,3 +1,7 @@
+if (Test-Path gmsv_mongo_win64.dll)
+{
+    rm gmsv_mongo_win64.dll
+}
 cargo build --release --target x86_64-pc-windows-msvc
 if (Test-Path target/x86_64-pc-windows-msvc/release/gmsv_mongo.dll)
 {
